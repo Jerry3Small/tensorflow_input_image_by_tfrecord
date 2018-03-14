86ec88c722f40d5a359f22109969aa49b7c26442
YJC (Jerry) <cjchang23@gmail.com>
2018-03-14 16:10:10 +0800

    [Jerry] Modify the img-convert python file for card images
     - The class label needs to be integer
     - Note that the image data is RGB data

--- src/jpg_to_mnist/convert-images-to-mnist-format.py
+++ src/jpg_to_mnist/convert-images-to-mnist-format.py
@@ -4,70 +4,76 @@ from array import *
 from random import shuffle
 
 # Load from and save to
-Names = [['./training-images','train'], ['./test-images','test']]
+Names = [['./training_images','train'], ['./testing_images','test']]
 
 for name in Names:
-	
-	data_image = array('B')
-	data_label = array('B')
 
-	FileList = []
-	for dirname in os.listdir(name[0])[1:]: # [1:] Excludes .DS_Store from Mac OS
-		path = os.path.join(name[0],dirname)
-		for filename in os.listdir(path):
-			if filename.endswith(".png"):
-				FileList.append(os.path.join(name[0],dirname,filename))
+    data_image = array('B')
+    data_label = array('B')
 
-	shuffle(FileList) # Usefull for further segmenting the validation set
+    FileList = []
 
-	for filename in FileList:
+    # NOTE: [1:] Excludes .DS_Store from Mac OS
+    for dirname in os.listdir(name[0]):
+        path = os.path.join(name[0],dirname)
+        for filename in os.listdir(path):
+            if filename.endswith(".png"):
+                FileList.append(os.path.join(name[0],dirname,filename))
 
-		label = int(filename.split('/')[2])
+    print (str(name) + " " + str(len(FileList)))
 
-		Im = Image.open(filename)
+    shuffle(FileList) # Usefull for further segmenting the validation set
 
-		pixel = Im.load()
+    for filename in FileList:
 
-		width, height = Im.size
+        # NOTE: The class labels have to be integer
+        label = int(filename.split('/')[2])
 
-		for x in range(0,width):
-			for y in range(0,height):
-				data_image.append(pixel[y,x])
+        # NOTE: Using grayscale mode
+        Im = Image.open(filename).convert('L')
 
-		data_label.append(label) # labels start (one unsigned byte each)
+        pixel = Im.load()
 
-	hexval = "{0:#0{1}x}".format(len(FileList),6) # number of files in HEX
+        width, height = Im.size
 
-	# header for label array
+        for x in range(0,width):
+            for y in range(0,height):
+                data_image.append(pixel[y,x])
 
-	header = array('B')
-	header.extend([0,0,8,1,0,0])
-	header.append(int('0x'+hexval[2:][:2],16))
-	header.append(int('0x'+hexval[2:][2:],16))
-	
-	data_label = header + data_label
+        data_label.append(label) # labels start (one unsigned byte each)
 
-	# additional header for images array
-	
-	if max([width,height]) <= 256:
-		header.extend([0,0,0,width,0,0,0,height])
-	else:
-		raise ValueError('Image exceeds maximum size: 256x256 pixels');
+    hexval = "{0:#0{1}x}".format(len(FileList),6) # number of files in HEX
 
-	header[3] = 3 # Changing MSB for image data (0x00000803)
-	
-	data_image = header + data_image
+    # header for label array
 
-	output_file = open(name[1]+'-images-idx3-ubyte', 'wb')
-	data_image.tofile(output_file)
-	output_file.close()
+    header = array('B')
+    header.extend([0,0,8,1,0,0])
+    header.append(int('0x'+hexval[2:][:2],16))
+    header.append(int('0x'+hexval[2:][2:],16))
 
-	output_file = open(name[1]+'-labels-idx1-ubyte', 'wb')
-	data_label.tofile(output_file)
-	output_file.close()
+    data_label = header + data_label
+
+    # additional header for images array
+
+    if max([width,height]) <= 256:
+        header.extend([0,0,0,width,0,0,0,height])
+    else:
+        raise ValueError('Image exceeds maximum size: 256x256 pixels');
+
+    header[3] = 3 # Changing MSB for image data (0x00000803)
+
+    data_image = header + data_image
+
+    output_file = open(name[1]+'-images-idx3-ubyte', 'wb')
+    data_image.tofile(output_file)
+    output_file.close()
+
+    output_file = open(name[1]+'-labels-idx1-ubyte', 'wb')
+    data_label.tofile(output_file)
+    output_file.close()
 
 # gzip resulting files
 
 for name in Names:
-	os.system('gzip '+name[1]+'-images-idx3-ubyte')
-	os.system('gzip '+name[1]+'-labels-idx1-ubyte')
+    os.system('gzip '+name[1]+'-images-idx3-ubyte')
+    os.system('gzip '+name[1]+'-labels-idx1-ubyte')
